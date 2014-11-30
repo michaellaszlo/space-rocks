df6d87e820282615e645dae60a28849105f748be
Michael Laszlo <git@michaellaszlo.com>
2014-11-29 23:58:59 -0500

    Keydown and keyup handling

--- js/space.rocks.js
+++ js/space.rocks.js
@@ -15,7 +15,7 @@ Sprite.make = function (center, size) {
 			context = Sprite.context;
 	sprite.draw = function (position, angle) {
 		context.translate(position.x, position.y);
-		context.rotate(angle);
+		context.rotate(Math.PI*angle/1800);
 		context.drawImage(Sprite.image, corner.x, corner.y, size.x, size.y,
 				-halfSize.x, -halfSize.y, size.x, size.y);
 		context.setTransform(1, 0, 0, 1, 0, 0);
@@ -26,18 +26,22 @@ Sprite.make = function (center, size) {
 SpaceRocks.keyToAction = {
   65: 'left', 68: 'right', 87: 'up'
 };
+SpaceRocks.activeKeys = {};
 
-SpaceRocks.keydown = function (event) {
+SpaceRocks.keyDownHandler = function (event) {
 	var global = SpaceRocks;
   var code = event.keyCode,
       action = global.keyToAction[code];
-  console.log(code);
-  if (action == 'left') {
-    console.log('left');
-  } else if (action == 'right') {
-    console.log('right');
-  } else if (action == 'up') {
-    console.log('up');
+  if (action !== undefined) {
+    global.activeKeys[action] = true;
+  }
+};
+SpaceRocks.keyUpHandler = function (event) {
+	var global = SpaceRocks;
+  var code = event.keyCode,
+      action = global.keyToAction[code];
+  if (action !== undefined) {
+    global.activeKeys[action] = false;
   }
 };
 
@@ -46,33 +50,63 @@ SpaceRocks.update = function () {
       canvas = global.canvas,
       context = global.context,
       ship = global.ship;
+  if (global.activeKeys['left'] && !global.activeKeys['right']) {
+    ship.angle -= 10;
+  } else if (global.activeKeys['right'] && !global.activeKeys['left']) {
+    ship.angle += 10;
+  }
   context.clearRect(0, 0, canvas.width, canvas.height);
-	ship.plainImage.draw(ship.pos, Math.PI/2);
+  global.tick += 1;
+  var seconds = Math.floor(global.tick);
+      minutes = Math.floor(seconds/60),
+      hours = Math.floor(minutes/60);
+  minutes %= 60;
+  seconds %= 60;
+  if (hours > 0) {
+    var timeString = hours + ':' + Math.floor(minutes/10) + minutes%10 + ':' +
+        Math.floor(seconds/10) + seconds%10;
+  } else if (minutes > 0) {
+    var timeString = minutes + ':' + Math.floor(seconds/10) + seconds%10;
+  } else {
+    var timeString = seconds;
+  }
+  context.fillText(timeString, 20, 30);
+	ship.plainImage.draw(ship.pos, ship.angle);
 };
 
 SpaceRocks.load = function () {
 	var global = SpaceRocks;
 
 	var canvas = document.getElementById('mainCanvas');
-	SpaceRocks.canvas = canvas;
+	global.canvas = canvas;
 	canvas.width = global.width;
 	canvas.height = global.height;
 	var canvasContainer = document.getElementById('mainCanvasContainer');
-	SpaceRocks.canvasContainer = canvasContainer;
+	global.canvasContainer = canvasContainer;
 	canvasContainer.style.width = global.width + 'px';
 	canvasContainer.style.height = global.height + 'px';
-	var context = Sprite.context = SpaceRocks.context = canvas.getContext('2d');
+	var context = Sprite.context = global.context = canvas.getContext('2d');
 
-	var ship = SpaceRocks.ship = {
+	var ship = global.ship = {
 		pos: {x: global.width / 2, y: global.height / 2},
 		vel: {x: 0, y: 0},
+    angle: 3600,
 		plainImage: Sprite.make({x: 70.5, y: 36.5}, {x: 135, y: 63}),
 		thrustImage: Sprite.make({x: 70.5, y: 108.5}, {x: 135, y: 63})
 	};
 
-  document.onkeydown = SpaceRocks.keydown;
+  document.onkeydown = global.keyDownHandler;
+  document.onkeyup = global.keyUpHandler;
+  context.fillStyle = '#ccc';
+  context.font = '20px sans-serif';
+  global.hertz = 60;
+  global.tick = 0;
+  global.updateIntervalID = window.setInterval(SpaceRocks.update,
+      1000/global.hertz);
+};
 
-  window.setInterval(SpaceRocks.update, 16.666666);
+SpaceRocks.stop = function () {
+  window.clearInterval(SpaceRocks.updateIntervalID);
 };
 
 window.onload = SpaceRocks.load;
